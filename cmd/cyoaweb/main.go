36e91331f28e1a81e9f56c5dda2f2eb5af52a191
conor.ney <conor.ux@gmail.com>
2023-01-05 00:30:10 -0500

    REFACTOR: moved JsonStory into its own function

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -18,10 +17,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("%+v\n", story)
 }
